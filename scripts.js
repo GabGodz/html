23682d7759d272cd1006c5132e578cd42b68fc8d
GabGodz <159465327+GabGodz@users.noreply.github.com>
2024-07-30 15:35:25 -0300

    Atualizar o scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
 
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    var message = document.getElementById('message');
+
+    if (username === 'admin' && password === 'admin') {
+        message.style.color = 'green';
+        message.textContent = 'Login bem-sucedido!';
+    } else {
+        message.style.color = 'red';
+        message.textContent = 'Usuário ou senha incorretos!';
+    }
+});
